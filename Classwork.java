bbf0a70511735c86af2ae946de20f7b625a7cd32
TinaD1906 <dotina87@gmail.com>
2024-10-01 10:10:09 -0700

    urmom

--- Classwork.java
+++ Classwork.java
@@ -1,7 +1,31 @@
+import java.util.Scanner;
+
 public class Classwork
 {
   public static void main(String[] args)
   {
-    // write your code
+    Scanner sc = new Scanner(System.in);  // a brand new scanner reading the keyboard
+    System.out.println("Hello, little child, what is your name?");
+    String name;
+    name = sc.nextLine();
+    System.out.println("Hello, " + name + ", how are you");
+
+    //if you nextInt() or nextDouble before a nextLine(), you will need an extra NextLine(); 
+  
+    System.out.println("What type of item are you buying?");
+    String item;
+    item = sc.nextLine();
+
+
+    System.out.println("How many are you buying?");
+    int amount;
+    amount = sc.nextInt();
+
+    System.out.println("How much does each one weigh?");
+    double weight;
+    weight = sc.nextDouble();
+
+    System.out.println( amount + " " + item + " at " + weight + " pounds each with weigh " + (weight*amount) + " each");
+
   }
 }
